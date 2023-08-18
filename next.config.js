9c929a9755745cc269194707a3e384c58ad17e84
Achal <achaldev3@gmail.com>
2023-08-18 21:25:43 +0530

    version-1.3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/multi-step-form",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
